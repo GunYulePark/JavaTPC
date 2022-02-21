7ecc3aca7f689061588a797bc57710bec9becdbe
GR <yule.gpark@gmail.com>
2022-02-21 15:23:36 +0900

    TPC08

---
+++ src/TPC08.java
@@ -0,0 +1,17 @@
+
+public class TPC08 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=30;
+		int b=50;
+		int v=sum(a, b);
+		System.out.println(v);
+	}
+	
+	public static int sum(int a, int b) {
+		int sum=a+b;
+		return sum;
+	}
+
+}
