aed1dd53f896d90a6322ce2437d9c1aae2f6ab76
GR <yule.gpark@gmail.com>
2022-02-27 15:47:50 +0900

    TPC15

--- src/TPC15.java
+++ src/TPC15.java
@@ -5,7 +5,10 @@ public class TPC15 {
 	public static void main(String[] args) {
 		BookDTO3 dto=new BookDTO3();
 		dto.setTitle("자바");
+		// 범위 넘어가서 안들어감 > 그러면 price==0
+		dto.setPrice(1000000);
+		
 		System.out.println(dto.getTitle());
+		System.out.println(dto.getPrice());
 	}
-
 }
--- src/kr/tpc/BookDTO3.java
+++ src/kr/tpc/BookDTO3.java
@@ -12,5 +12,35 @@ public class BookDTO3 {
 	public String getTitle() {
 		return title;
 	}
+	
+	public void setPrice(int price) {
+//		너무 큰 값이 들어가면 안들어가도록 문지기
+		if(price>=1000000) {
+		} else {
+			this.price=price; // 여기서 this는 BookDTO3를 가리키는 듯하다.
+		}
+		
+	}
+	
+	public int getPrice() {
+		if (price==0) {
+			return -1;
+		} else {
+			return price;
+		}
 
+	}
+	public String getCompany() {
+		return company;
+	}
+	public void setCompany(String company) {
+		this.company = company;
+	}
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	
 }
