97c7f2890b4e396452207f46fe20b0ba2ade9154
GR <yule.gpark@gmail.com>
2022-02-28 15:22:58 +0900

    TPC19

---
+++ src/TPC19.java
@@ -0,0 +1,13 @@
+
+public class TPC19 {
+
+	public static void main(String[] args) {
+//		1. Java에서 제공해주는 class들.. API
+//		String이라는 class
+//		String 하위에 있는 method를 이용
+		String str=new String("APPLE");
+		System.out.println(str.toLowerCase());
+		
+	}
+
+}
