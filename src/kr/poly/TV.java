496cccd6653b05d89a8a960290dd992281f85706
GR <yule.gpark@gmail.com>
2022-04-11 10:32:10 +0900

    TPC27

--- src/kr/poly/TV.java
+++ src/kr/poly/TV.java
@@ -1,12 +1,26 @@
 package kr.poly;
 
 public class TV implements RemoCon{
+	int currCH=70;
 	public void chUp() {
-		System.out.println("TV 채널이 올라간다.");
+		if(currCH<RemoCon.MAXCH) {
+			//MAXCH는 RemoCon에서 static 변수이기 때문에 RemoCon.MAXCH으로 접근.
+			currCH++;
+		} else {
+			currCH=RemoCon.MINCH;
+		}
+		System.out.println("TV 채널이 올라간다.:"+currCH);
+		
 	}
 	public void chDown() {
-		System.out.println("TV 채널이 내려간다.");
+		if(currCH>RemoCon.MINCH) {
+			currCH--;
+		} else {
+			currCH=RemoCon.MAXCH;
+		}
+		System.out.println("TV 채널이 내려간다.:"+currCH);
 	}
+	
 	public void internet() {
 		System.out.println("인터넷이 된다.");
 	}
