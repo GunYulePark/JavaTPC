44eae1f98776b50f9c492f80a0be02d676a00f8d
GR <yule.gpark@gmail.com>
2022-04-25 23:53:07 +0900

    TPC31

--- src/TPC31.java
+++ src/TPC31.java
@@ -3,6 +3,7 @@ import kr.inflearn.MyClass;
 public class TPC31 {
 
 	public static void main(String[] args) {
+//	MyClass 사용해보기
 		MyClass my=new MyClass();
 		int h=my.sum(1, 2);
 		System.out.println(h);
